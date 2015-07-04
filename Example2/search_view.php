1c66fdf033ffe378161c6f60ebf67874049225b3
jadenjwlee <jaden.jwlee@gmail.com>
2015-07-03 21:43:44 -0400

    Linke - modify

--- Example2/search_view.php
+++ Example2/search_view.php
@@ -1,51 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>MediaWiki API Search</title>
-        <script type="text/javascript" src="js/libs/jquery-2.1.3.min.js"></script>
-        <script type="text/javascript" src="js/libs/bootstrap.min.js"></script>
-        <link href="css/style.css" rel="stylesheet">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    </head>
-    <body>
-        <!-- Start navigation bar -->
-        <div class="navbar navbar-style navbar-fixed-top">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#"><img src="img/logo.png" /></a>
-                <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </a>
-            </div>
-
-            <div class="navbar-collapse collapse">
-                <ul  class="nav navbar-nav" class=" nav navbar-nav">
-                    <li >
-                        <a href="../Example1/index.html" target="_top" >Example 1</a>
-                    </li>
-                    <li class="active">
-                        <a href="../Example2/index.php"  target="_top" >Example 2</a>
-                    </li>
-                     <li>
-                        <a href="../Example3/wiki.php"  target="_top" >Exampl 3</a>
-                    </li>
-                    <li >
-                        <a href="../Example4/index.php"  target="_top" >Example 4</a>
-                    </li>
-                </ul>
-
-            </div>
-        </div>
-            
         <div id="page">
-            <div id="header"><h1>MediaWiki API Search (xml)</h1></div>
+<!--            <div id="header"><h1>MediaWiki API Search (xml)</h1></div>-->
             <div id="main">
 
                 <br /><br />
-                <form action="." method="post">
+                <form action="wiki.php" method="post">
                     <b>Search</b>
                     <input type="text" name="query" value="<?php echo $query; ?>"/>
                     <input type="submit" value="Search"/>
@@ -70,5 +28,3 @@
 
             </div><!-- end main -->
         </div><!-- end page -->
-    </body>
-</html>
--- Example2/wiki.php
+++ Example2/wiki.php
@@ -10,7 +10,7 @@
       <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <a class="navbar-brand" href="../index.html">Home</a>
+            <a class="navbar-brand" href="../index.html">Home</a>
           <a href="../Example1/index.html" target="_top" class="navbar-brand" href="#">Example 1</a>
           <a href="../Example2/wiki.php" target="_top" class="navbar-brand" href="#">Example 2</a>
           <a href="../Example3/wiki.php" target="_top" class="navbar-brand" href="#">Example 3</a>
@@ -20,9 +20,9 @@
     </nav><!-- /.navbar -->
     <div class="container">
         <div class="row">
-<h2>Wikipedia API. This example will demonstrate you how get categories from Wikipedia on particular keyword. I will be using "bread" keyword for this tutorial.</h2>
-<p>Firstly, we need to think about the query or API url. Here is the simple example, just put this link in your url and see the result</p>
-  <p>The result will be:</p><pre>https://en.wikipedia.org/w/api.php?action=query&list=allcategories&rawcontinue=&acprefix=bread&format=json</pre>
+<h2>The MediaWiki web API is a web service that provides convenient access to wiki features, data, and meta-data over HTTP.</h2>
+<p>This URL tells English Wikipedia's web service API to send you the content of the main page:</p>
+  <p>The result will be:</p><pre>https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=xml</pre>
   <p>Let's break down this query:<br />
      This URL is the base URL for English Wikipedia's API. Every query should start from this URL:
     <pre>https://en.wikipedia.org/w/api.php?</pre>  
@@ -35,57 +35,49 @@
     Search for all category titles that begin with this value:
     <pre>acprefix=bread</pre>
     The format of output values. Can be json,xml and many more:
-    <pre>format=json</pre>  
+    <pre>format=xml</pre>  
   </p><br />
   <p>You can play wil different variations of query in wikiAPI <a href="https://en.wikipedia.org/wiki/Special:ApiSandbox">Sandbox</a></p>
   <p>Next, for this example I used CURL request to get data from Wikipedia</p>
-  <pre>function callWiki($url)
-    {
-    //initialize
-    $ch = curl_init();
-    //Set the opions
-    curl_setopt_array($ch, array(
-    //specify the url
-    CURLOPT_URL => $url,
-    //Return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
-    CURLOPT_RETURNTRANSFER => true,
-    //Stop cURL from verifying the peer's certificate
-    CURLOPT_SSL_VERIFYPEER => false,
-    //Check the existence of a common name and also verify that it matches the hostname provided. 
-    CURLOPT_SSL_VERIFYHOST => 2
-    ));
-    //execute and fetch the result
-    $result = curl_exec($ch);
-    //free up the curl handle
-    curl_close($ch);
-    //return fetched information
-    return $result;
-    }</pre>
+  <pre>function search_wiki($query) {
+    // Set up the URL for the query
+    $query = urlencode($query);
+    $base_url = 'https://en.wikipedia.org/w/api.php';
+    $params = 'action=query&prop=revisions&rvprop=content&format=xml&titles=' . $query;
+    $url = $base_url . '?' . $params;
+    
+    // Use cURL to get data in JSON format
+    $api = simplexml_load_file($url);
+
+    return $api;
+    }
+  </pre>
   <p>So we call the function, $api is our query</p>
   <pre>
     //call function
-    $api_response = callWiki($api);
     //Next we transfer result into php array with json_decode function
-    $results = json_decode($api_response, true);</pre>
+    $api = search_wiki($query);</pre>
   <p>Finally we just loop through array to get all articalls that starts with bread</p>
-  <pre><code>foreach ($results['query']['allcategories'] as $k => $v)
-        {
-        foreach($v as $key => $val)
-            {
-            //Replace all white spaces with underscore
-            $string = str_replace(' ', '_', $val);
-    echo "&lthref='http://www.wikipedia.org/wiki/$string'&gt".$val."&lt/a&gt"; 
- . "&ltbr /&gt";
+  <pre><code>
+        foreach ($api->query as $qy) {
+            foreach ($qy->pages as $pgs) {
+                foreach ($pgs->page as $pg) {
+                    foreach ($pg->revisions as $revs) {
+                        echo  htmlentities($revs->rev) ;
+                    }
+                }
             }
-}</code></pre> 
+        }
+</code></pre> 
   <div class="text-center">
       <h4>The result:</h4>
       <pre>
 <?php 
-include 'get.php';
+    include 'index.php';
 ?>
 </pre>
-      <h3><a href="get.php" download>Download Code</a></h3>
+      <h3><a href="index.php" download>Download index.php</a></h3>
+      <h3><a href="search_view.php" download>Download search_view.php</a></h3>
   </div>
   <div>
   </div>
